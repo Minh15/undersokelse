382e6c588aee9d59f36a84da041ceba1875e668d
Minh15 <minht@viken.no>
2022-01-02 17:28:25 +0100

    Noe

--- minReise/static/javascript.js
+++ minReise/static/javascript.js
@@ -1,17 +1,17 @@
 
 let data = [
     {sted: "Poland", pris: 3000, dato: new Date("2020-05-15"), url: "https://gfx.nrk.no/2WPJvxq7jhP4mVU6cFp8sg-qM2GSJgABGqZP_46dOKGw"},
-    {sted: "Poland", pris: 2700, dato: new Date("2020-07-27"), url: "https://upload.wikimedia.org/wikipedia/commons/7/75/Logo_TVE-2.svg"},
-    {sted: "Tyskland", pris: 2700, dato: new Date ("2019-08-12"), url: "https://upload.wikimedia.org/wikipedia/commons/d/dd/France_3_2018.svg"},
-    {sted: "Tyskland", pris: 5200, dato: new Date("2019-09-04"), url: "https://www.infobilder.com/bilde-nummer-5-dl20176.jpg"},
+    {sted: "Tyskland", pris: 2700, dato: new Date("2020-07-27"), url: "https://upload.wikimedia.org/wikipedia/commons/7/75/Logo_TVE-2.svg"},
+    {sted: "England", pris: 3450, dato: new Date ("2019-08-12"), url: "https://upload.wikimedia.org/wikipedia/commons/d/dd/France_3_2018.svg"},
+    {sted: "Frankrike", pris: 5200, dato: new Date("2019-09-04"), url: "https://www.infobilder.com/bilde-nummer-5-dl20176.jpg"},
     {sted: "USA", pris: 7400, dato: new Date ("2016-04-18"), url: "https://img.pixers.pics/pho_wat(s3:700/FO/57/91/04/80/700_FO57910480_30ce9f91636f63e81087a8ca165fda36.jpg,700,700,cms:2018/10/5bd1b6b8d04b8_220x50-watermark.png,over,480,650,jpg)/klistremerker-brann-alfabet-nummer-4-fire.jpg.jpg"},
-    {sted: "USA", pris: 6800, dato: new Date ("2016-05-05"), url: "https://www.infobilder.com/bilde-nummer-5-dl20176.jpg"}
+    {sted: "Singapore", pris: 6800, dato: new Date ("2016-05-05"), url: "https://www.infobilder.com/bilde-nummer-5-dl20176.jpg"}
 ]
 
 let navbar = [
     {name:"Inbex", href: "./index.html"},
     {name:"Feriebilder", href: "./jaja.html"},
-    {name:"hjemme3", href: "./bildesortering.html"},
+    {name:"Transportmidler", href: "./bildesortering.html"},
     {name:"side4", href: "./brukerInput.html"}
 ]
 
@@ -19,27 +19,26 @@ let navbar = [
 let knapper = [
     {
         bilMerke: [
-            {Modell1: "", src:"https://www.cdn.tv2.no/images/13951498.jpg?imageId=13951498&x=0&y=0&cropw=99.90574929312&croph=100&width=1060&height=597"},
-            {Modell2: "", src:""},
-            {Modell3: "", src:""}
+            {Modell1: "Expedition", src:"https://th.bing.com/th/id/R.524ae5107a8affa4ff62a90c84c0cf53?rik=E1ZtCL3KB%2bM31w&pid=ImgRaw&r=0"},
+            {Modell2: "Mustang", src:"https://th.bing.com/th/id/OIP.RMkDIgkDjyNLm1P46kVoLwHaD1?pid=ImgDet&rs=1"},
+            {Modell3: "Edge", src:"https://www.motortrend.com/uploads/sites/10/2015/11/2014-ford-edge-se-suv-angular-front.png"}
         ]
     },
     {
         bilMerke: [
-            {Modell1: "", src:"https://bavaria.no/globalassets/biler/bmw/bmw-i4/i4-ikon.png"},
-            {Modell2: "", src:""},
-            {Modell3: "", src:""}
+            {Modell1: "Camry", src:"https://groupeparkavenue.com/sites/default/files/assets/trim/primary_image/2018/Toyota/Camry/cc_2018TOC020013_01_1280_0040_0.png"},
+            {Modell2: "Corolla", src:"https://th.bing.com/th/id/OIP.gyoE82EQwmfASrlNPo7JIQHaEL?pid=ImgDet&rs=1"},
+            {Modell3: "Venza", src:"https://cacsw.org/wp-content/uploads/2017/12/2019-Toyota-Venza-white-color-on-white-background-front-exterior-view-hd-wallpaper-1024x768.jpg"}
         ]
     },
     {
         bilMerke: [
-            {Modell1: "", src:"https://www-europe.nissan-cdn.net/content/dam/Nissan/nissan_europe/hp_cmstest/New_HomepagePush_1440x510px_New_QQ.jpg.ximg.l_full_m.smart.jpg"},
-            {Modell2: "", src:""},
-            {Modell3: "", src:""}
+            {Modell1: "Rio", src:"https://toddsofcampsie.com/wp-content/uploads/2020/02/kia-rio_2017-1-blaze-red_featuredimage.png"},
+            {Modell2: "Soul", src:"https://www.motortrend.com/uploads/sites/10/2017/02/2017-kia-soul-exclaim-hatchback-angular-front.png"},
+            {Modell3: "Sorento", src:"https://leblogauto.b-cdn.net/wp-content/uploads/2020/03/2020_Kia-Sorento_07.jpg"}
         ]
     },
 ]
-
 
     function prisSortering(data) {
         // let prisListe = [];
